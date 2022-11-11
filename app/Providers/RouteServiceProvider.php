6539e698af787acf0daaf56020826239b87ccc69
valk20 <vkond97@gmail.com>
2022-11-12 02:05:59 +0300

    Revert "auth redirect change"
    This reverts commit 9c3aa9a01d03ad77e4fbb3b9f04de3f66dad5203.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,8 +19,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
-    public const INDEX = '/';
-
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      *
